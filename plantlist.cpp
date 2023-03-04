1bd7466d5ea9a9bc21bc16333a23bba0eb179135
Miles Grant <milesgrant132@gmail.com>
2023-03-04 13:36:07 -0800

    Init commit
    cpp and h file for the plant list

---
+++ plantlist.cpp
@@ -0,0 +1,63 @@
+//Implementation of the plant list
+//Author: Miles G Date: March 4th
+#include "plantlist.h"
+#include <iostream>
+#include <algorithm>
+
+using std::cout;
+using std::endl;
+
+plantlist::plantlist(){
+    size = 0;
+}
+
+
+plantlist::~plantlist(){
+
+}
+
+bool plantlist::addPlant(plant plant_to_add){
+    auto iter_available = std::find(availablePlants.begin(),availablePlants.end(), plant_to_add);
+    auto iter_past = std::find(pastPlants.begin(),pastPlants.end(), plant_to_add);
+    if(iter_available != availablePlants.end() && iter_past != pastPlants.end()){
+        availablePlants.emplace_back(plant_to_add);
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+
+bool plantlist::removePlant(plant plant_to_remove){
+
+}
+
+plant plantlist::movePlant(int find_ID){
+    auto iter = std::find_if(availablePlants.begin(),availablePlants.end(), [](const plant& obj){
+        return obj.ID.compare(find_ID) == 0;    
+    });
+    if(iter != availablePlants.end()){
+        pastPlants.emplace_back(availablePlants.at(iter));
+
+    }
+}
+
+void plantlist::printAvailable(){
+    
+}
+
+void plantlist::printAdtopted(){
+    
+}
+
+
+int plantlist::sizeAvailable(){
+    
+}
+
+int plantlist::sizePast(){
+    
+}
+
+
---
+++ plantlist.h
@@ -0,0 +1,25 @@
+//header file for plantlist object
+//Author: Miles G Date: march 4th
+#include <iostream>
+#include <vector>
+
+#define plant int
+using std::vector;
+
+class plantlist{
+public:
+    plantlist();
+    ~plantlist();
+    bool addPlant(plant plant_to_add);
+    plant movePlant(int id);
+    bool removePlant(plant plant_to_remove);
+    void printAvailable();
+    void printAdtopted();
+    int sizeAvailable();
+    int sizePast();
+
+private:
+    int size;
+    vector<plant> availablePlants;
+    vector<plant> pastPlants;   
+};
