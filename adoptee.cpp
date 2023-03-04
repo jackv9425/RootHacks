44292dd6ee0b2f255091375055d7fea564e323a3
claireshou <cshou@outlook.com>
2023-03-04 11:33:59 -0800

    adoptee w/ some method implementations

--- adoptee.cpp
+++ adoptee.cpp
@@ -6,57 +6,66 @@
 using std::string;
 using std::cout;
 using std::endl;
+using std::cin;
 using std::vector;
 
-// default constructor
-adoptee::adoptee(string input_name, int hours)
+// constructor
+Adoptee::Adoptee(string input_name, int hours)
 {
     name = input_name;
     amount_sunlight = hours;
-    //vectors here
-
     amount_plants = 0;
-    desired_plants = 0;
-}
-
-// destructor
-adoptee::~adoptee()
-{
-
+    amount_desired = 0;
 }
 
 // inserts the name of a desired plant into the list
-void adoptee::desiredPlant(string st)
+void Adoptee::desiredPlant(string st)
 {
+    desired_plants.push_back(st);
+    amount_desired++;
 
 }
 
 // adopt: adopts the plant by inserting into the list
-void adoptee::adopt(Plant p)
+void Adoptee::adopt(int id)
 {
-
+    Plant p = movePlant(id);
+    plants_owned.push_back(p);
+    amount_plants++;
 }
 
 // based on desired_plants, finds a matching plant and returns it
-Plant adoptee::findMatch() const
+void Adoptee::findMatch() const
 {
+    string str;
+    cout << "Enter the plant you are looking for:\n";
+    cin >> str;
+    
+    int id = pl.check(str);
 
+    if(id == -1) {
+        //do something
+    }
+    else{
+        adopt(id);
+    }
+    //
 }
 
 // returns the name of the adoptee
-string adoptee::getName() const
+string Adoptee::getName() const
 {
-
+    return name;
 }
 
 //returns the amount of sunlight the adoptee can give
-int adoptee::amountSun() const
+int Adoptee::amountSun() const
 {
-
+    return amount_sunlight;
 }
 
 // return the number of plants this person owns
-int adoptee::amount_owned() const
+int Adoptee::amount_owned() const
 {
-
+    return amount_plants;
 }
--- adoptee.h
+++ adoptee.h
@@ -9,23 +9,28 @@ using std::cout;
 using std::endl;
 using std::vector;
 
-class adoptee() {
+class Adoptee {
 public:
     // METHODS
     // default constructor
-    adoptee(string input_name, int hours);
-    // destructor
-    ~adoptee();
+    Adoptee(string input_name, int hours);
+
     // inserts the name of a desired plant into the list
     void desiredPlant(string st);
+
     // adopt: adopts the plant by inserting into the list
-    void adopt(Plant p);
+    void adopt(int id);
+
     // based on desired_plants, finds a matching plant and returns it
-    Plant findMatch() const;
+    // n = amount of plants in the available plants list
+    void findMatch(int n) const;
+
     // returns the name of the adoptee
     string getName() const;
+
     //returns the amount of sunlight the adoptee can give
     int amountSun() const;
+
     // return the number of plants this person owns
     int amount_owned() const;
 
@@ -35,5 +40,5 @@ private:
     vector<string> desired_plants;
     int amount_sunlight;
     int amount_plants; //number of plants this person owns
-    int desired_plants; //number of plants this person wants
+    int amount_desired; //number of plants this person wants
 };
